702506c6ac9492b4b6cd19d08159333433d50bf6
Mario Avila <avilamario16@gmail.com>
2015-12-22 16:44:13 -0600

    Modified Article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,46 @@
 class ArticlesController < ApplicationController
+
   def new
+    @article = Artilce.new(article_params)
   end
 
   def create
-  end
-
-  def update
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
   end
 
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :subtitle, :image, :category_id, :content, :author_id)
   end
 end
