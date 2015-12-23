1ee8216c94331e2a77c02a057a5b4fb213f16db4
Mario Avila <avilamario16@gmail.com>
2015-12-23 01:51:36 -0600

    Added relationship-combobox to all forms

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,22 @@
 module ArticlesHelper
+  def category_id_select
+    names = Category.all
+      categories_all = []
+
+      names.each do |t|
+        categories_all << [t.name, t.id]
+      end
+      categories_all
+  end
+
+  def author_id_select
+    names = Author.all
+      authors_all = []
+
+      names.each do |t|
+        authors_all << [t.name, t.id]
+      end
+      authors_all
+  end
+  
 end
--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,2 +1,11 @@
 module SectionsHelper
+  def author_id_select
+    names = Author.all
+      authors_all = []
+
+      names.each do |t|
+        authors_all << [t.name, t.id]
+      end
+      authors_all
+  end
 end
