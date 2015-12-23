470c6ae141a03181061977af1e49f2b3cbad487f
Mario Avila <avilamario16@gmail.com>
2015-12-23 01:26:47 -0600

    Added some seeds for pseudo-testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+authors = Author.create([{name: 'Mario', image: 'mario.jpg'}, {name: 'Jose', image: 'jose.jpg'}])
+categories = Category.create([{name: 'Experiencias', image: 'experiencias.jpg'}, {name: 'Beber', image: 'beber.jpg' }])
+sections = Section.create([{name: 'Super tecno', author_id: 1, image: 'supertecno.jpg'}, {name: 'Vida y cosas', author_id: 2, image: 'vidaycosas.jpg'} ])
+articles = Article.create([{title: 'Hoy soy robot', subtitle: 'Deberas deberitas', image: 'hoysoyrobot.jpg', category_id: 1, content: 'Hoy desperte y me di cuenta que era un robot', author_id: 1}, {title: 'Esta bebida rifa', subtitle: 'Diez de diez', image: 'bebidarifa.jpg', category_id: 2, content: 'Esta bebida si que rifa, diez de diez', author_id: 2}])
+comments = Comment.create([{article_id: 1, name: 'JuanchoPanza', content: 'Eso que dices no te lo creo'},{article_id: 2, name: 'Jujuguito', content: 'A mi tambien me encanta esa bebida'}])
