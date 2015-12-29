fbebcb583e8ea8217664d275b846fcdd9dfe0df9
Mario Avila <avilamario16@gmail.com>
2015-12-29 17:13:39 -0600

    Added validations to Section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,8 @@
 class Section < ActiveRecord::Base
+  #~relationships
   belongs_to :author
-
+  #~validations
+  validates :name, presence: true, length: { minimum: 3}
+  #~imageUploader
   mount_uploader :image, SectionUploader
 end
