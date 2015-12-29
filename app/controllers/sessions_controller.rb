3d5982554bb719125e7b55cdabc13fc9d5ce7092
Mario Avila <avilamario16@gmail.com>
2015-12-29 16:02:11 -0600

    Added sessions controller and helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new
+  end
+
+  def create
+    user = User.find_by(mail: params[:session][:mail].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to :home
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to :home
   end
 end
