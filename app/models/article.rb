e478b94dfa38832d8b387ec590177b8f51adf248
Mario Avila <avilamario16@gmail.com>
2015-12-29 17:02:20 -0600

    Added validations to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,12 @@
 class Article < ActiveRecord::Base
+  #~relationships
   belongs_to :author
   belongs_to :category
   has_many :comments
+  #~
+  validates :title, presence: true, length: { minimum: 3}
+  validates :subtitle, presence: true, length: { minimum: 3}
+  validates :content, presence: true, length: { minimum: 3}
 
   mount_uploader :image, ArticleUploader
 end
