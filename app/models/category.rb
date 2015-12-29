97f6b505cc8b856baeb47c3b93c57b28dd54b045
Mario Avila <avilamario16@gmail.com>
2015-12-29 17:10:36 -0600

    Added validations to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
+  #~relationships
   has_many :articles
-
+  #~validations
+  validates :name, presence: true, length: { minimum: 3}
+  #~imageUploader
   mount_uploader :image, CategoryUploader
 end
